258ee5d0e22b276cab5d58df734a18cad62b023b
Arthur Landim <landim.arthur@gmail.com>
2017-10-02 22:18:41 -0300

    Test: insert and select test for UDT types

--- src/Cassandra.IntegrationTests/Linq/LinqTable/LinqUdtTests.cs
+++ src/Cassandra.IntegrationTests/Linq/LinqTable/LinqUdtTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Cassandra.Data.Linq;
+using Cassandra.IntegrationTests.Linq.Structures;
 using Cassandra.IntegrationTests.TestBase;
 using Cassandra.IntegrationTests.TestClusterManagement;
 using Cassandra.Mapping;
@@ -17,11 +18,6 @@ namespace Cassandra.IntegrationTests.Linq.LinqTable
         private readonly string _uniqueKeyspaceName = TestUtils.GetUniqueKeyspaceName();
         private readonly Guid _sampleId = Guid.NewGuid();
 
-        private Table<Album> GetAlbumTable()
-        {
-            return new Table<Album>(_session, new MappingConfiguration().Define(new Map<Album>().TableName("albums")));
-        }
-
         public override void OneTimeSetUp()
         {
             if (CassandraVersion < Version.Parse("2.1.0"))
@@ -33,70 +29,111 @@ namespace Cassandra.IntegrationTests.Linq.LinqTable
             _session.Execute(String.Format(TestUtils.CreateKeyspaceSimpleFormat, _uniqueKeyspaceName, 1));
             _session.ChangeKeyspace(_uniqueKeyspaceName);
             _session.Execute("CREATE TYPE song (id uuid, title text, artist text)");
+            _session.Execute("CREATE TYPE album (id uuid, name text, publishingdate timestamp, songs list<frozen<song>>)");
             _session.UserDefinedTypes.Define(UdtMap.For<Song>());
+            _session.UserDefinedTypes.Define(UdtMap.For<Album>());
         }
 
         [Test, TestCassandraVersion(2, 1, 0)]
         public void LinqUdt_Select()
         {
             // Avoid interfering with other tests
-            _session.Execute("DROP TABLE IF EXISTS albums");
-            _session.Execute("CREATE TABLE albums (id uuid primary key, name text, songs list<frozen<song>>, publishingdate timestamp)");
+            _session.Execute("DROP TABLE IF EXISTS band");
+            _session.Execute("CREATE TABLE band (id uuid primary key, name text, albuns list<frozen<album>>)");
             _session.Execute(
                 new SimpleStatement(
-                    "INSERT INTO albums (id, name, songs) VALUES (?, 'Legend', [{id: uuid(), title: 'Africa Unite', artist: 'Bob Marley'}])",
+                    "INSERT INTO band (id, name, albuns) VALUES (?, 'Ben Folds Five', [" +
+                    "{id: uuid(), name:'Whatever and Ever Amen', publishingdate: toTimestamp(now()), songs: [{id: uuid(), title: 'Kate', artist: 'Ben Folds Five'}," +
+                    "{id: uuid(), title: 'Brick', artist: 'Ben Folds Five'}]}])",
                     _sampleId));
 
-            var table = new Table<Album>(_session, new MappingConfiguration().Define(new Map<Album>().TableName("albums")));
-            var album = table.Select(a => new Album { Id = a.Id, Name = a.Name, Songs = a.Songs }).Execute().First();
-            Assert.AreEqual(_sampleId, album.Id);
-            Assert.AreEqual("Legend", album.Name);
-            Assert.NotNull(album.Songs);
-            Assert.AreEqual(1, album.Songs.Count);
-            var song = album.Songs[0];
-            Assert.AreEqual("Africa Unite", song.Title);
-            Assert.AreEqual("Bob Marley", song.Artist);
+            var table = new Table<Band>(_session, new MappingConfiguration().Define(new Map<Band>().TableName("band")));
+            var band = table.Select(a => new Band { Id = a.Id, Name = a.Name, Albuns = a.Albuns }).Execute().First();
+            Assert.AreEqual(_sampleId, band.Id);
+            Assert.AreEqual("Ben Folds Five", band.Name);
+            Assert.NotNull(band.Albuns);
+            Assert.AreEqual(1, band.Albuns.Count);
+            Assert.AreEqual(2, band.Albuns[0].Songs.Count);
+            Assert.NotNull(band.Albuns[0].PublishingDate);
+            var song1 =  band.Albuns[0].Songs[0];
+            var song2 =  band.Albuns[0].Songs[1];
+            Assert.AreEqual("Kate", song1.Title);
+            Assert.AreEqual("Ben Folds Five", song1.Artist);
+            Assert.AreEqual("Brick", song2.Title);
+            Assert.AreEqual("Ben Folds Five", song2.Artist);
         }
 
         [Test, TestCassandraVersion(2,1,0)]
         public void LinqUdt_Insert()
         {
             // Avoid interfering with other tests
-            _session.Execute("DROP TABLE IF EXISTS albums");
-            _session.Execute("CREATE TABLE albums (id uuid primary key, name text, songs list<frozen<song>>, publishingdate timestamp)");
+            _session.Execute("DROP TABLE IF EXISTS band");
+            _session.Execute("CREATE TABLE band (id uuid primary key, name text, albuns list<frozen<album>>)");
 
-            var table = GetAlbumTable();
+            var table = new Table<Band>(_session, new MappingConfiguration().Define(new Map<Band>().TableName("band")));
             var id = Guid.NewGuid();
             var album = new Album
             {
-                Id = id,
-                Name = "Mothership",
-                PublishingDate = DateTimeOffset.Parse("2010-01-01"),
+                Id = Guid.NewGuid(),
+                Name = "The Colour and the Shape",
+                PublishingDate = DateTimeOffset.Parse("1997-05-20"),
                 Songs = new List<Song>
                 {
                     new Song
                     {
                         Id = Guid.NewGuid(),
-                        Artist = "Led Zeppelin",
-                        Title = "Good Times Bad Times"
+                        Artist = "Foo Fighters",
+                        Title = "Monkey Wrench"
                     },
                     new Song
                     {
                         Id = Guid.NewGuid(),
-                        Artist = "Led Zeppelin",
-                        Title = "Communication Breakdown"
+                        Artist = "Foo Fighters",
+                        Title = "Everlong"
                     }
                 }
             };
-            table.Insert(album).Execute();
+            var band = new Band
+            {
+                Id = id,
+                Name = "Foo Fighters",
+                Albuns = new List<Album>()
+            };
+            band.Albuns.Add(album);
+            table.Insert(band).Execute();
             //Check that the values exists using core driver
-            var row = _session.Execute(new SimpleStatement("SELECT * FROM albums WHERE id = ?", id)).First();
-            Assert.AreEqual("Mothership", row.GetValue<object>("name"));
-            var songs = row.GetValue<List<Song>>("songs");
+            var row = _session.Execute(new SimpleStatement("SELECT * FROM band WHERE id = ?", id)).First();
+            Assert.AreEqual("Foo Fighters", row.GetValue<object>("name"));
+            var albuns = row.GetValue<List<Album>>("albuns");
+            Assert.NotNull(albuns);
+            Assert.AreEqual(1, albuns.Count);
+            var songs = albuns[0].Songs;
             Assert.NotNull(songs);
             Assert.AreEqual(2, songs.Count);
-            Assert.NotNull(songs.FirstOrDefault(s => s.Title == "Good Times Bad Times"));
-            Assert.NotNull(songs.FirstOrDefault(s => s.Title == "Communication Breakdown"));
+            Assert.NotNull(songs.FirstOrDefault(s => s.Title == "Monkey Wrench"));
+            Assert.NotNull(songs.FirstOrDefault(s => s.Title == "Everlong"));
         }
+
+//        [Test, TestCassandraVersion(2, 1, 0)]
+//        public void LinqUdt_Select_Enum()
+//        {
+//            // Avoid interfering with other tests
+//            _session.Execute("DROP TABLE IF EXISTS someClass");
+//            _session.Execute("CREATE TABLE someClass (id uuid primary key, name text, someUdt list<frozen<someUdt>>)");
+//            _session.Execute(
+//                new SimpleStatement(
+//                    "INSERT INTO someClass (id, name, someUdt) VALUES (?, 'Legend', [{someEnum: 'ONE'}])",
+//                    _sampleId));
+//
+//            var table = new Table<SomeClass>(_session, new MappingConfiguration().Define(new Map<SomeClass>().TableName("someClass")));
+//            var album = table.Select(a => new SomeClass { Id = a.Id, Name = a.Name, SomeUdt = a.SomeUdt}).Execute().First();
+//            Assert.AreEqual(_sampleId, album.Id);
+//            Assert.AreEqual("Legend", album.Name);
+//            Assert.NotNull(album.SomeUdt);
+//            Assert.AreEqual(1, album.SomeUdt.Count);
+//            var someUdt = album.SomeUdt[0];
+//            Assert.AreEqual(SomeEnum.ONE, someUdt.SomeEnum);
+//        }
+        
     }
 }
